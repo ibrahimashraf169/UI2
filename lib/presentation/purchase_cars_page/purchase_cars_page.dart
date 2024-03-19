4892e6409a697d7a549a822ee4ecc6d8bb66cbb2
ibrahimashraf169 <ibrahimasharf12@gmail.com>
2024-03-20 00:13:12 +0200

    sign up ui done login in ui done sign up auth completed login in auth is done spare parts ui done and its navigation maintenance service ui done and its navigation sell cars ui 90 done

--- lib/presentation/purchase_cars_page/purchase_cars_page.dart
+++ lib/presentation/purchase_cars_page/purchase_cars_page.dart
@@ -1,16 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ibrahim_s_application1/core/app_export.dart';
+import 'package:ibrahim_s_application1/widgets/custom_bottom_bar.dart';
+import 'package:ibrahim_s_application1/widgets/custom_elevated_button.dart';
+import 'package:ibrahim_s_application1/widgets/custom_outlined_button.dart';
+import 'package:ibrahim_s_application1/widgets/custom_search_view.dart';
 import '../purchase_cars_page/widgets/listingcard_item_widget.dart';
 import '../purchase_cars_page/widgets/servicelist_item_widget.dart';
 import '../purchase_cars_page/widgets/seventynine_item_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:ibrahim_s_application1/core/app_export.dart';
-import 'package:ibrahim_s_application1/widgets/custom_search_view.dart';
 
-// ignore_for_file: must_be_immutable
 class PurchaseCarsPage extends StatelessWidget {
-  PurchaseCarsPage({Key? key})
-      : super(
-          key: key,
-        );
+  PurchaseCarsPage({Key? key}) : super(key: key);
 
   TextEditingController searchController = TextEditingController();
 
@@ -19,9 +18,8 @@ class PurchaseCarsPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Container(
+        body: SizedBox(
           width: double.maxFinite,
-          decoration: AppDecoration.fillOnPrimary,
           child: Column(
             children: [
               SizedBox(height: 49.v),
@@ -30,80 +28,61 @@ class PurchaseCarsPage extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.only(bottom: 5.v),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Align(
-                          alignment: Alignment.center,
-                          child: CustomSearchView(
-                            width: 272.h,
-                            controller: searchController,
-                            hintText: "Find a Project",
-                            alignment: Alignment.center,
-                          ),
+                        CustomSearchView(
+                          width: 272.h,
+                          controller: searchController,
+                          hintText: "Find a Project",
                         ),
                         SizedBox(height: 25.v),
-                        _buildServiceList(context),
+                        _buildFrameRow(context),
                         SizedBox(height: 26.v),
-                        Padding(
-                          padding: EdgeInsets.only(left: 20.h),
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                height: 22.adaptSize,
-                                width: 22.adaptSize,
-                                child: Stack(
-                                  alignment: Alignment.topRight,
-                                  children: [
-                                    CustomImageView(
-                                      imagePath: ImageConstant.imgContrast,
-                                      height: 22.adaptSize,
-                                      width: 22.adaptSize,
-                                      alignment: Alignment.center,
-                                    ),
-                                    CustomImageView(
-                                      imagePath: ImageConstant.imgPlus,
-                                      height: 12.adaptSize,
-                                      width: 12.adaptSize,
-                                      alignment: Alignment.topRight,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(
-                                  left: 6.h,
-                                  top: 3.v,
-                                ),
-                                child: Text(
-                                  "Click to sell your car",
-                                  style: CustomTextStyles.labelLargeBlue600
-                                      .copyWith(
-                                    decoration: TextDecoration.underline,
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20.h),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  height: 22.adaptSize,
+                                  width: 22.adaptSize,
+                                  child: Stack(
+                                    alignment: Alignment.topRight,
+                                    children: [
+                                      CustomImageView(
+                                        imagePath: ImageConstant.imgContrast,
+                                        height: 22.adaptSize,
+                                        width: 22.adaptSize,
+                                        alignment: Alignment.center,
+                                      ),
+                                      CustomImageView(
+                                        imagePath: ImageConstant.imgPlus,
+                                        height: 12.adaptSize,
+                                        width: 12.adaptSize,
+                                        alignment: Alignment.topRight,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ),
-                            ],
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 6.h,
+                                    top: 3.v,
+                                  ),
+                                  child: Text(
+                                    "Click to sell your car",
+                                    style: CustomTextStyles.labelLargeBlue600
+                                        .copyWith(
+                                      decoration: TextDecoration.underline,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                        SizedBox(height: 19.v),
-                        Padding(
-                          padding: EdgeInsets.only(left: 20.h),
-                          child: Text(
-                            "Most Popular Cars",
-                            style: CustomTextStyles.titleSmallWhiteA700,
-                          ),
-                        ),
-                        _buildSeventyNine(context),
-                        SizedBox(height: 19.v),
-                        Padding(
-                          padding: EdgeInsets.only(left: 20.h),
-                          child: Text(
-                            "Available Cars",
-                            style: CustomTextStyles.titleSmallWhiteA700,
-                          ),
-                        ),
-                        SizedBox(height: 17.v),
-                        _buildListingCard(context),
+                        SizedBox(height: 23.v),
+                        _buildProductCard(context),
                       ],
                     ),
                   ),
@@ -112,80 +91,91 @@ class PurchaseCarsPage extends StatelessWidget {
             ],
           ),
         ),
+        // Remove the bottom navigation bar from here
       ),
     );
   }
 
-  /// Section Widget
-  Widget _buildServiceList(BuildContext context) {
-    return SizedBox(
+  Widget _buildFrameRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildSparePartsButton(context),
+        _buildMaintenanceServicesButton(context),
+        SizedBox(
+          width: 95.h, // Adjust the width here
+          child: _buildSellCarsButton(context),
+        ),
+      ],
+    );
+  }
+
+
+
+
+
+
+  Widget _buildSparePartsButton(BuildContext context) {
+    return CustomOutlinedButton(
+      width: 95.h,
+      text: "Spare Parts",
+      buttonStyle: CustomButtonStyles.outlinePrimary,
+      onPressed: () {
+        Navigator.pushNamed(context, AppRoutes.mainviewScreen);
+      },
+    );
+  }
+
+  Widget _buildMaintenanceServicesButton(BuildContext context) {
+    return CustomOutlinedButton(
+      width: 187.h,
+      text: "Maintenance Services",
+      buttonStyle: CustomButtonStyles.outlinePrimary,
+      onPressed: () {
+        Navigator.pushNamed(context, AppRoutes.maintenanceServicesScreen);
+      },
+    );
+  }
+
+  Widget _buildSellCarsButton(BuildContext context) {
+    return CustomElevatedButton(
       height: 36.v,
+      width: 187.h,
+      text: "Old Cars",
+      buttonStyle: CustomButtonStyles.fillPrimaryTL18,
+      buttonTextStyle: CustomTextStyles.labelLargeGray200,
+      onPressed: () {
+        // Do nothing as we're already on the maintenance services screen.
+      },
+    );
+  }
+
+  Widget _buildProductCard(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 14.h),
       child: ListView.separated(
-        scrollDirection: Axis.horizontal,
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
         separatorBuilder: (
-          context,
-          index,
-        ) {
-          return SizedBox(
-            width: 12.h,
+            context,
+            index,
+            ) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0.v),
+            child: SizedBox(
+              width: 265.h,
+              child: Divider(
+                height: 1.v,
+                thickness: 1.v,
+                color: appTheme.blue600,
+              ),
+            ),
           );
         },
-        itemCount: 4,
+        itemCount: 3,
         itemBuilder: (context, index) {
-          return ServicelistItemWidget();
+          return ListingcardItemWidget(); // Change this to the desired widget
         },
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildSeventyNine(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerRight,
-      child: SizedBox(
-        height: 137.v,
-        child: ListView.separated(
-          padding: EdgeInsets.only(left: 20.h),
-          scrollDirection: Axis.horizontal,
-          separatorBuilder: (
-            context,
-            index,
-          ) {
-            return SizedBox(
-              width: 1.h,
-            );
-          },
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return SeventynineItemWidget();
-          },
-        ),
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildListingCard(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.h),
-        child: ListView.separated(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          separatorBuilder: (
-            context,
-            index,
-          ) {
-            return SizedBox(
-              height: 10.v,
-            );
-          },
-          itemCount: 2,
-          itemBuilder: (context, index) {
-            return ListingcardItemWidget();
-          },
-        ),
       ),
     );
   }
